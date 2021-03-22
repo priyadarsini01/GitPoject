dabf80da06e69b745a11076047dd713ee49b5490
LENOVO <LENOVO@DESKTOP-DFEID15>
2021-03-22 21:23:59 +0530

    First commit....

---
+++ src/prject1.java
@@ -0,0 +1,15 @@
+
+public class prject1 {
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("eating somthing");
+		}
+		else
+		{
+			System.out.println("do work");
+		}
+	}
+
+}
